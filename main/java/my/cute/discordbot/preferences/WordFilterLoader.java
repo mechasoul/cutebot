b2e7eb0bd01f9c5f7cf5c788bfdbfa1ab8f57f05
mecha <mechasoul@mechasoulindustries.gov.ca>
2018-12-05 23:00:37 -0800

    use filenotfoundexception instead of file.isFile() checks

--- main/java/my/cute/discordbot/preferences/WordFilterLoader.java
+++ main/java/my/cute/discordbot/preferences/WordFilterLoader.java
@@ -1,9 +1,11 @@
 package my.cute.discordbot.preferences;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 import my.cute.discordbot.IdUtils;
+import my.cute.discordbot.bot.demo;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.LineIterator;
@@ -15,41 +17,46 @@ public class WordFilterLoader {
 	
 	public static WordFilter load(long id) {
 		WordFilter filter = new WordFilter(id);
-		File filterFile = new File("./markovdb/" + id + "/wordfilter.ini");
-		if(filterFile.isFile()) {
-			try (LineIterator it = FileUtils.lineIterator(filterFile, "UTF-8")) {
-				while(it.hasNext()) {
-					String line = it.nextLine();
-					String tokens[] = line.split("=",2);
-					String option = tokens[0];
-					String value = tokens[1];
-					
-					switch(option) {
-						case "bannedwords":
-							if(!value.equals("[empty]")) filter.addFilteredWords(value);
-							break;
-						case "filteraction":
-							filter.setFilterResponse(value);
-							break;
-						case "compiledfilter":
-							if(!value.equals("[null]")) filter.setCompiledFilter(value);
-							break;
-						case "isusingexplicitregex":
-							filter.setExplicitRegexStatus(Boolean.parseBoolean(value));
-							break;
-						default:
-							logger.warn("unknown option encountered in PreferencesLoader.load() for guild "
-									+ IdUtils.getFormattedServer(id) + ". line: " + line);
-							break;
-					}
+		try (LineIterator it = FileUtils.lineIterator(new File("./markovdb/" + id + "/wordfilter.ini"), "UTF-8")) {
+			while(it.hasNext()) {
+				String line = it.nextLine();
+				String tokens[] = line.split("=",2);
+				String option = tokens[0];
+				String value = tokens[1];
+
+				//TODO make sure we dont allow the special flags to be saved as filter words in wordfilter
+				//update commands in commandfactory
+				switch(option) {
+					case "bannedwords":
+						if(!value.equals("[empty]")) filter.addFilteredWords(value);
+						break;
+					case "filteraction":
+						filter.setFilterResponse(value);
+						break;
+					case "compiledfilter":
+						if(!value.equals("[null]")) filter.setCompiledFilter(value);
+						break;
+					case "isusingexplicitregex":
+						filter.setExplicitRegexStatus(Boolean.parseBoolean(value));
+						break;
+					default:
+						logger.warn("unknown option encountered in PreferencesLoader.load() for guild "
+								+ IdUtils.getFormattedServer(id) + ". line: " + line);
+						break;
 				}
-			} catch (IOException e) {
-				logger.error("couldn't build wordfilter for guild " + IdUtils.getFormattedServer(id)
-						+ " in WordFilterLoader.load(). fatal error");
-				e.printStackTrace();
-				System.exit(1);
 			}
-		} 
+		} catch (FileNotFoundException ex) {
+			//first run on this server
+			//WordFilter(long) constructor initialises default values
+			//so nothing more needs to be done to it
+			logger.info("created new WordFilter object with default values for new guild "
+					+ IdUtils.getFormattedServer(id) + ". proceeding...");
+		} catch (IOException e) {
+			logger.error("couldn't build wordfilter for guild " + IdUtils.getFormattedServer(id)
+					+ " in WordFilterLoader.load(). fatal error");
+			e.printStackTrace();
+			demo.exit(1);
+		}
 		return filter;
 	}
 }
