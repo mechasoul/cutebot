9afbf208103b9385b70fec42dd5dc4ac0cb427ab
mecha <mechasoul@mechasoulindustries.gov.ca>
2018-12-05 22:59:25 -0800

    add/remove server methods for use on guild join/leave

--- main/java/my/cute/discordbot/commands/PermissionsManager.java
+++ main/java/my/cute/discordbot/commands/PermissionsManager.java
@@ -1,9 +1,8 @@
 package my.cute.discordbot.commands;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -32,7 +31,6 @@ public class PermissionsManager {
 			StringBuilder sb = new StringBuilder();
 			entry.getValue().forEach(userId -> sb.append(userId + "\r\n"));
 			try {
-				Files.createDirectories(Paths.get("./markovdb/" + entry.getKey()));
 				FileUtils.writeStringToFile(new File("./markovdb/" + entry.getKey() + "/admins.ini"), 
 						sb.toString().trim(), "UTF-8");
 			} catch (IOException e) {
@@ -61,39 +59,6 @@ public class PermissionsManager {
 		serverToUserPerms.clear();
 		defaultGuilds.clear();
 		
-		for(Guild g : demo.getJda().getGuilds()) {
-			Long serverId = g.getIdLong();
-			serverToUserPerms.put(serverId, new HashSet<Long>(8));
-			try (LineIterator permIterator = FileUtils.lineIterator(new File("./markovdb/" + serverId + "/admins.ini"), "UTF-8")) {
-				//each line has id of an admin in that server
-				while(permIterator.hasNext()) {
-					Long userId = Long.parseLong(permIterator.nextLine());
-					if(!userToServerPerms.containsKey(userId)) {
-						userToServerPerms.put(userId,  new HashSet<Long>(8));
-					}
-					userToServerPerms.get(userId).add(serverId);
-					serverToUserPerms.get(serverId).add(userId);
-					
-				}
-				//empty admins.ini check
-				//other problems will throw an exception but this would fail quietly
-				//shouldnt ever happen, but maybe in exceptional circumstances
-				if(serverToUserPerms.get(serverId).isEmpty()) {
-					serverToUserPerms.get(serverId).add(g.getOwner().getUser().getIdLong());
-					logger.warn("admins.ini was empty for server " 
-							+ IdUtils.getFormattedServer(serverId) + "! added owner as admin, "
-							+ "proceeding...");
-				}
-					
-				
-			} catch (IOException | NumberFormatException e) {
-				logger.error("couldn't build permissions object for guild "
-						+ IdUtils.getFormattedServer(serverId) + " in PermissionsManager.load(). fatal error: "
-						+ e.getMessage());
-				e.printStackTrace();
-				System.exit(1);
-			} 
-		}
 		//construct default server object
 		//holds the default server for each user so they can choose not to specify server if there's only 1 they use
 		//format for each line is <user id>,<server id>
@@ -102,11 +67,55 @@ public class PermissionsManager {
 				String words[] = defaultIterator.nextLine().split(",");
 				defaultGuilds.put(Long.parseLong(words[0]), Long.parseLong(words[1]));
 			}
+		} catch (FileNotFoundException ex) {
+			//first run initialisation
+			//nothing really has to happen here
+			//default guilds for owners will be initialised as admins.ini check below happens
+			//defaultguilds.ini will be created on save() call
+			logger.warn("PermissionsManager.load(): defaultguilds.ini not found! will be created on next save, "
+					+ "proceeding...");
 		} catch (IOException | NumberFormatException e) {
 			logger.error("couldn't build default server object in PermissionsManager.load(). fatal error");
 			e.printStackTrace();
 			System.exit(1);
 		}
+		
+		demo.getJda().getGuilds().forEach(PermissionsManager::loadServer);
+		
+	}
+	
+	private static void loadServer(Guild g) {
+		Long serverId = g.getIdLong();
+		serverToUserPerms.put(serverId, new HashSet<Long>(4));
+		try (LineIterator permIterator = FileUtils.lineIterator(new File("./markovdb/" + serverId + "/admins.ini"), "UTF-8")) {
+			//each line has id of an admin in that server
+			while(permIterator.hasNext()) {
+				Long userId = Long.parseLong(permIterator.nextLine());
+				addAdmin(userId, serverId);		
+			}
+			//empty admins.ini check
+			//other problems will throw an exception but this would fail quietly
+			//shouldnt ever happen, but maybe in exceptional circumstances
+			if(serverToUserPerms.get(serverId).isEmpty()) {
+				addAdmin(g.getOwner().getUser().getIdLong(), serverId);
+				logger.warn("admins.ini was empty for server " 
+						+ IdUtils.getFormattedServer(serverId) + "! added owner as admin, "
+						+ "proceeding...");
+			}	
+		} catch (FileNotFoundException ex) {
+			//first run initialisation/load after new server join here
+			//similar to above, add owner as admin. admins.ini will be created at next save
+			addAdmin(g.getOwner().getUser().getIdLong(), serverId);
+			logger.info("admins.ini not found for (new?) server " 
+					+ IdUtils.getFormattedServer(serverId) + "! added owner as admin, "
+					+ "proceeding...");
+		} catch (IOException | NumberFormatException e) {
+			logger.error("PermissionsManager.loadServer(long): couldn't build permissions object for guild "
+					+ IdUtils.getFormattedServer(serverId) + ". fatal error: "
+					+ e.getMessage());
+			e.printStackTrace();
+			demo.exit(1);
+		} 
 	}
 	
 	//check if user has admin permission over the specified server
@@ -163,7 +172,7 @@ public class PermissionsManager {
 	
 	//gives specified user admin powers over specified server
 	//returns true if permission was new, or false if they already were admin
-	public static boolean addAdmin(long userId, long serverId) {
+	public static boolean addAdmin(Long userId, Long serverId) {
 		boolean added = false;
 		//devs don't get added as admin as they have permissions everywhere
 		if(isDeveloper(userId)) return false;
@@ -173,7 +182,7 @@ public class PermissionsManager {
 			HashSet<Long> servers = new HashSet<Long>(4);
 			servers.add(serverId);
 			userToServerPerms.put(userId, servers);
-			if(!defaultGuilds.containsKey(userId)) defaultGuilds.put(userId,  serverId);
+			defaultGuilds.putIfAbsent(userId,  serverId);
 			added = true;
 		} else {
 			if(userToServerPerms.get(userId).add(serverId)) {
@@ -186,31 +195,26 @@ public class PermissionsManager {
 		return added;
 	}
 	
-	//removes specified user's admin powers over specified server
-	//returns true if privileges were successfully removed, or false if not (eg doesnt exist)
-	public static boolean removeAdmin(long userId, long serverId) {
-		boolean removed = false;
+	//for general admin removal, use this
+	//don't typically want to remove server owner
+	public static boolean removeAdmin(Long userId, Long serverId) {
 		
 		//server owner can't be removed
 		if(demo.getJda().getGuildById(serverId).getOwner().getUser().getIdLong() == userId) {
 			return false;
 		}
 		
-		//need the following
-		//remove server id from user id's hashset and return true
-		//if set now empty, remove mapping 
-		//if not present, return false
-		//if key not in map, return false
+		return forceRemoveAdmin(userId, serverId);
+	}
+	
+	//removes specified user's admin powers over specified server
+	//returns true if privileges were successfully removed, or false if not (eg doesnt exist)
+	public static boolean forceRemoveAdmin(Long userId, Long serverId) {
+		boolean removed = false;
 		
-		
-		//has check-then-act race condition
-		//same thing everywhere else in my code
-		//change it sometime maybe
-		//check perms map. if user is absent, can't do anything
 		if(!userToServerPerms.containsKey(userId)) {
 			return false;
 		} else {
-			//possible race condition here leading to null pointer
 			HashSet<Long> servers = userToServerPerms.get(userId);
 			//check their servers for the given id. remove it if it's there
 			if(servers.remove(serverId)) {
@@ -231,6 +235,31 @@ public class PermissionsManager {
 		}
 		
 		return removed;
+	}
+	
+	public static void addServer(Guild g) {
+		loadServer(g);
+	}
+	
+	//note this cleans entries from maps, but not off disk
+	//also note this doesn't change defaultguilds set to the current server
+	//i don't want to iterate every defaultguild on server leave so i'd have to change
+	//defaultguilds to a per-server basis to remove them here
+	//so for now check if we can just account for it in the actual defaultguild check in commandmanager
+	public static void removeServer(Guild g) {
+		Long serverId = g.getIdLong();
+		HashSet<Long> admins = serverToUserPerms.get(serverId);
+		//remove server from all its admins' lists
+		admins.forEach(userId -> removeAdmin(userId, serverId));
+		forceRemoveAdmin(g.getOwner().getUser().getIdLong(), serverId);
+		if(!admins.isEmpty()) {
+			logger.warn("PermissionsManager.removeServer(Guild): removed admins and owner "
+					+ "from admin set for server " + IdUtils.getFormattedServer(serverId)
+					+ ", but it isn't empty? will clear it now. admins: " + admins.toString());
+		}
+		admins.clear();
+		//and remove server from map altogether
+		serverToUserPerms.remove(serverId);
 	}
 	
 	//should never be in a server without a corresponding entry in serverToUserPerms
--- main/java/my/cute/discordbot/preferences/PreferencesManager.java
+++ main/java/my/cute/discordbot/preferences/PreferencesManager.java
@@ -1,9 +1,11 @@
 package my.cute.discordbot.preferences;
 
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
 import my.cute.discordbot.bot.demo;
+import net.dv8tion.jda.core.entities.Guild;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,6 +15,7 @@ public class PreferencesManager {
 	private static final Map<Long, GuildPreferences> guildPreferences = new ConcurrentHashMap<Long, GuildPreferences>(10, 0.8f, 4);
 	@SuppressWarnings("unused")
 	private static final Logger logger = LoggerFactory.getLogger(PreferencesManager.class);
+	private static final Set<Long> guildsModifyingDiscChannels = ConcurrentHashMap.newKeySet();
 	
 	public static void load() {
 		for(Long id : demo.getGuildIdLongs()) {
@@ -26,8 +29,29 @@ public class PreferencesManager {
 			.forEach(entry -> entry.getValue().saveToDisk());
 	}
 	
+	public static void addServer(Guild g) {
+		GuildPreferences prefs = PreferencesLoader.load(g.getIdLong());
+		guildPreferences.putIfAbsent(g.getIdLong(), prefs);
+	}
+	
+	public static boolean removeServer(Guild g) {
+		return (guildPreferences.remove(g.getIdLong()) != null);
+	}
+	
 	public static GuildPreferences getGuildPreferences(long id) {
 		return guildPreferences.get(id);
+	}
+	
+	public static boolean addGuildModifyingDiscChan(long id) {
+		return guildsModifyingDiscChannels.add(id);
+	}
+	
+	public static boolean isGuildModifyingDiscChan(long id) {
+		return guildsModifyingDiscChannels.contains(id);
+	}
+	
+	public static boolean removeGuildModifyingDiscChan(long id) {
+		return guildsModifyingDiscChannels.remove(id);
 	}
 	
 }
